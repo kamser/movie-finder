d0bb29b9df6a3b25792197d78217f4bf2f6ca943
kamser <keylor.morataya@gmail.com>
2026-04-27 19:57:20 -0700

    Finish with Web app presenting the movies using the mocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
 import './App.css'
+import validMovieResponse from '../mocks/validMovieResponse.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const hasMovies = validMovieResponse.Search !== null
+  const movies = hasMovies
+                  ? validMovieResponse.Search
+                  : null
 
   return (
     <>
       <h1>Best Movie Finder</h1>
       <header>
         <form action="">
-          <input name="userQuery" type="text" />
+          <input name="userQuery" type="text" placeholder='Avengers, Matrix, Titanic, Avatar...'/>
           <button type='sumbit'>Search</button>
         </form>
       </header>
       <main>
         <h2>Movies</h2>
+        {hasMovies 
+              ? movies.map((element) => (
+                    <ul key={element.imdbID}>
+                      <li>
+                        <h3>{element.Title}</h3>
+                        <p>{element.Year}</p>
+                        <img src={element.Poster} alt={`Its an image from the movie ${element.Title}`}></img>
+                      </li>
+                    </ul>
+                  )
+                )
+              :
+              <p>No movies found</p>
+        }
       </main>
     </>
   )
