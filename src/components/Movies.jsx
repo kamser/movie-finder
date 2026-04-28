783164629104fcc36849d99ee444df2ccc2c7e40
kamser <keylor.morataya@gmail.com>
2026-04-27 19:58:07 -0700

    Added the component movies without finishing the refactor

---
+++ src/components/Movies.jsx
@@ -0,0 +1,44 @@
+import validMovieResponse from '../mocks/validMovieResponse.json'
+import movieNotFoundResponse from '../../mocks/movieNotFoundResponse.json'
+function ListOfMovies({movies}){
+    return(
+        <>
+            <ul>
+            {
+                movies.map((element) => ( 
+                    <li key={element.imdbID}>
+                    <h3>{element.Title}</h3>
+                    <p>{element.Year}</p>
+                    <img src={element.Poster} alt={`Its an image from the movie ${element.Title}`}></img>
+                    </li>      
+                  ))
+            }
+            </ul>
+        </>  
+    )
+}
+
+function NoMoviesFound({errorMessage}){
+    return (
+        <>
+            <p>{errorMessage}</p>
+        </>
+    )
+}
+
+export function Movies(){
+    const hasMovies = validMovieResponse.Search !== null
+    const movies = hasMovies
+                  ? validMovieResponse.Search
+                  : null
+    const errorMessage = movieNotFoundResponse.Error
+    return(
+        <>
+            {
+                hasMovies
+                    ? <ListOfMovies movies={movies}></ListOfMovies>
+                    : <NoMoviesFound errorMessage={errorMessage}></NoMoviesFound>
+            }           
+        </>
+    )
+}
