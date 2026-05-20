7151fe3400c6668c8daf9b3ee72efac6ec808870
kamser <keylor.morataya@gmail.com>
2026-05-19 21:02:56 -0700

    Fixed the issue with the search errors due a named parameter

--- validator/search.js
+++ validator/search.js
@@ -2,12 +2,12 @@ import validator from 'zod'
 
 const searchSchema = validator.object(
     {
-        userSearch: validator.string()
+        search: validator.string()
                                 .min(1, 'This field can not be empty')
                                 .regex(/^[A-Za-z0-9 ]+$/, 'This field must be a word or a sentence')
     }
 )
 
-export function validateSearch({userSearch}){
-    return searchSchema.safeParse({userSearch})
+export function validateSearch({search}){
+    return searchSchema.safeParse({search})
 }
